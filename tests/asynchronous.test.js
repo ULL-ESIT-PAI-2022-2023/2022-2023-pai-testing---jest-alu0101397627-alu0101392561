1eafdc9cb9672f7c2e628a96fffe27996337b622
Alexander Gonzalez Covic <ag.covic@gmail.com>
2023-02-05 17:41:34 +0000

    Async finished

---
+++ tests/asynchronous.test.js
@@ -0,0 +1,17 @@
+const apiExample = require('../src/asynchronous');
+const dataExample = {PAI: '2022-2023'};
+
+test('API Example 1', () => {
+  return apiExample().then(data => {
+    expect(data).toEqual(dataExample);
+  });
+});
+
+test('API Example 2 - async function', async () => {
+  const data = await apiExample();
+  expect(data).toEqual(dataExample);
+})
+
+test('API Example 3 - resolves', async () => {
+  await expect(apiExample()).resolves.toEqual(dataExample);
+})
