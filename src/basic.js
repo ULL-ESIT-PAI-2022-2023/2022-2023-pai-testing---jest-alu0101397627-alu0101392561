555df7570576494691fa06b3e8315b70e326753b
usuario <alu0101392561@ull.edu.es>
2023-02-06 11:45:19 +0000

    Test

---
+++ src/basic.js
@@ -0,0 +1,4 @@
+function sum(a, b) { /// A simple example of function
+  return a + b;
+}
+module.exports = sum; /// We use de module.exports in order to export the contain of this file to the order one
