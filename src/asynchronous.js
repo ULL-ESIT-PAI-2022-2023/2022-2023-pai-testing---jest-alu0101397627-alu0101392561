5bbc70dfbcfaee4c79f030088a9e737b2bf4c9f6
Alexander Gonzalez Covic <ag.covic@gmail.com>
2023-02-08 18:15:50 +0000

    Update Async

--- src/asynchronous.js
+++ src/asynchronous.js
@@ -1,7 +1,18 @@
+// const examplePromise = new Promise( (resolve, reject) => {
+//   const x = 5;
+//   if (x === 5) { return resolve("Ok!"); }
+//   else { return reject('An error has occurred.'); }
+// });
+
+// examplePromise().then(
+//   (value) => { console.log(value); },
+//   (error) => { console.log('An error has occurred.'); }
+// );
+
 const apiExample = async () => {
   let url = `https://run.mocky.io/v3/1f9b74cf-bbcf-463d-a45b-72c71b55840a`;
   let promise = await fetch(url);
-  return await promise.json();
+  return promise.json();
 }
 
 module.exports = apiExample
--- tests/03-async/asynchronous.test.js
+++ tests/03-async/asynchronous.test.js
@@ -2,9 +2,9 @@ const apiExample = require('../../src/asynchronous');
 const dataExample = {PAI: '2022-2023'};
 
 test('API Example 1', () => {
-  return apiExample().then(data => {
-    expect(data).toEqual(dataExample);
-  });
+  return apiExample().then(
+    (data) => { expect(data).toEqual(dataExample); }
+  );
 });
 
 test('API Example 2 - async function', async () => {
