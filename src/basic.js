1965de61ccad4049c22bd537cf8f5765aba7463e
Alexander Gonzalez Covic <ag.covic@gmail.com>
2023-02-08 15:48:48 +0000

    Mock and Setup Teardown

--- src/basic.js
+++
@@ -1,4 +0,0 @@
-function sum(a, b) { /// A simple example of function
-  return a + b;
-}
-module.exports = sum; /// We use de module.exports in order to export the contain of this file to the order one
