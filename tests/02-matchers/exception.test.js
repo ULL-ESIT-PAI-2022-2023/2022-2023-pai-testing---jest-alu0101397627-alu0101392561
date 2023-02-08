c55d62bc61d947c2cd6fdea17e6bbb4ffa0a48df
Alexander Gonzalez Covic <ag.covic@gmail.com>
2023-02-08 15:36:43 +0000

    Fix test name

--- tests/02-matchers/exception.test.js
+++ tests/02-matchers/exception.test.js
@@ -3,7 +3,7 @@ const errorFunction = () => {
   throw new Error('An error has occurred.');
 } 
 
-test('Divide two numbers', () => {
+test('Error example', () => {
   expect(() => errorFunction()).toThrow();
   expect(() => errorFunction()).toThrow(/error/);
   expect(() => errorFunction()).toThrow('An error has occurred.');
