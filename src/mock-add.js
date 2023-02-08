9818d4be7be8b11331dca0efe2a5ad5154505eb2
usuario <alu0101392561@ull.edu.es>
2023-02-08 15:42:10 +0000

    Archivo mock

--- src/mock-add.js
+++ src/mock-add.js
@@ -2,4 +2,4 @@
 const doAdd = (a, b, callback) => {
   callback(a + b); /// callback is a type of function that recibes another one as a parameter, in this case acts like a return
 };
-module.exports = doAdd;
+module.exports = doAdd; 
