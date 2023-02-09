099ead8656a95f957467251b969c9a92f6d1dcbf
usuario <alu0101392561@ull.edu.es>
2023-02-09 18:54:09 +0000

    changes in mock

--- tests/mock-test/functions-mock.test.js
+++ tests/mock-test/functions-mock.test.js
@@ -1,5 +1,4 @@
 const doAdd = require('../../src/mock-add');
-
 test("calls callback with arguments added", () => {
   const mockCallback = jest.fn(); /// Creating the object
   doAdd(1, 2, mockCallback); 
--- tests/mock-test/manipulation-mock.test.js
+++ tests/mock-test/manipulation-mock.test.js
@@ -1,18 +1,14 @@
 const getRandomNumber = require('../../src/mock');
-
-const randomNumberExpected = 0.123456789 /// we create a possible result of calling the function
-
+const RANDOM_NUMBER_EXPECTED = 0.123456789 /// we create a possible result of calling the function
 beforeEach(() => {
   // In the before each, we use the funcionalities of the mocks function to emulate the random number using jest.spy on
-  jest.spyOn(global.Math, 'random').mockReturnValue(randomNumberExpected);
+  jest.spyOn(global.Math, 'random').mockReturnValue(RANDOM_NUMBER_EXPECTED);
 });
-
 afterEach(() => {
   // We restore de original result
   jest.spyOn(global.Math, 'random').mockRestore();
 });
-
 test('it should return a random value', () => {
   // Because of the before each, we know the result, so the test will be succesfull
-  expect(getRandomNumber()).toBe(randomNumberExpected);
+  expect(getRandomNumber()).toBe(RANDOM_NUMBER_EXPECTED);
 });
