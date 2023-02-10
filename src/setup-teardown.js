1eec5511c57d86ec33250eb498627d0bfc6ea3be
usuario <alu0101392561@ull.edu.es>
2023-02-10 17:20:34 +0000

    Setup and teardown

--- src/setup-teardown.js
+++ src/setup-teardown.js
@@ -1,20 +1,16 @@
 class ListClass { /// Class example
   constructor() { /// Constructor
     this.list = [];
-  }
-  
+  } 
   insert(element) { /// method for insert elements
     this.list.push(element);
   }
- 
   reset() { /// method for erase all elements
     this.list = [];
   }
-
   getList() { /// getter of the list
     return this.list;
   }
-
   print() { /// method for print the list
     for (let i = 0; i < this.list.length; ++i) {
       console.log(this.list[i]);
