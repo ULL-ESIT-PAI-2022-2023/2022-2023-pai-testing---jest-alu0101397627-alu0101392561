0017a65f9e63539218759733a69bb7ed75f1f183
Alexander Gonzalez Covic <ag.covic@gmail.com>
2023-02-11 16:27:16 +0000

    Removed Mock and Async

--- src/mock-add.js
+++
@@ -1,5 +0,0 @@
-/// Use of mock with callback
-const doAdd = (firstOperand, secondOperand, callback) => {
-  callback(firstOperand + secondOperand); /// callback is a type of function that recibes another one as a parameter, in this case acts like a return
-};
-module.exports = doAdd; 
