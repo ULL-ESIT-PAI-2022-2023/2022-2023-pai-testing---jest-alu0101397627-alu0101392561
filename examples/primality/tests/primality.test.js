5f9034f98fcaa1c87ffd921c9c5874bc23b6802f
Alexander Gonzalez Covic <ag.covic@gmail.com>
2023-02-11 14:45:07 +0000

    Better example primality

--- examples/primality/tests/primality.test.js
+++ examples/primality/tests/primality.test.js
@@ -2,7 +2,7 @@ const prime = require('../src/primality');
 
 // Timeout for not async functions
 // https://github.com/facebook/jest/issues/11543
-const testTimeoutInMs = 5000; // 5 seconds
+const testTimeoutInMs = 5000;  // 5 seconds
 let testStartTime;
 
 beforeEach(() => {
@@ -16,7 +16,6 @@ afterEach(() => {
 });
 
 describe('Check if a number is prime', () => {
-
   test('977 is prime.', () => {
     expect(prime.primeNumberFast(977)).toBeTruthy();
     expect(prime.primeNumberSlow(977)).toBeTruthy();
@@ -27,11 +26,17 @@ describe('Check if a number is prime', () => {
     expect(prime.primeNumberSlow(1)).toBeFalsy();
   });
 
-  test('Check if large number is prime in time - Slower Version', () => {
-    expect(prime.primeNumberSlow(370248451)).toBeTruthy();
-  }); 
+  test(
+      `Check if large number is prime in less than ${
+          testTimeoutInMs}ms - Slower Version`,
+      () => {
+        expect(prime.primeNumberSlow(370248451)).toBeTruthy();
+      });
 
-  test('Check if large number is prime in time - Faster Version', () => {
-    expect(prime.primeNumberFast(370248451)).toBeTruthy();
-  });
+  test(
+      `Check if large number is prime in less than ${
+          testTimeoutInMs}ms - Faster Version`,
+      () => {
+        expect(prime.primeNumberFast(370248451)).toBeTruthy();
+      });
 });
